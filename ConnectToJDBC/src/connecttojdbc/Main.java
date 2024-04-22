ad1555ca693a53aa28a9d1ce0c1fdcdaaf31520c
Niall McGuinness <niall.mcguinness@dkit.ie>
2024-04-22 10:32:52 +0100

    :tada: Initial commit

---
+++ ConnectToJDBC/src/connecttojdbc/Main.java
@@ -0,0 +1,22 @@
+
+package connecttojdbc;
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        
+        //setup password and user name stuff
+             
+        //setup DB url stuff
+        
+        //connect to DB
+        
+        //execute query
+        
+        //disconnect from DB
+    }
+}
+
+
+
