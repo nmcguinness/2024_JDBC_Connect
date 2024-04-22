04dfa82e8bbbdc481acfd955472aec3d961fb7bc
Niall McGuinness <niall.mcguinness@dkit.ie>
2024-04-22 10:40:51 +0100

    Update Main.java

--- ConnectToJDBC/src/connecttojdbc/Main.java
+++ ConnectToJDBC/src/connecttojdbc/Main.java
@@ -3,12 +3,16 @@ package connecttojdbc;
 
 public class Main {
 
-    public static void main(String[] args) {
-        
+   public static void main(String[] args) {        
         
         //setup password and user name stuff
+        String password = "";
+        String userName =  "root";
+        
              
         //setup DB url stuff
+        String dbURL = "jdbc:mysql://localhost:3306/";
+        String dbName = "games"; //pick name that you have
         
         //connect to DB
         
